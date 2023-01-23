eddf89eccc7f5407dcc2330cc3c75b13cae4fff5
drgio <giorgiobuttiglieri99@gmail.com>
2023-01-23 23:55:59 +0100

    Add first draft of microservice

---
+++ IT/BE/EMSP/BookingManagementService/src/main/java/it/polimi/emall/emsp/bookingmanagementservice/controller/TestController.java
@@ -0,0 +1,28 @@
+package it.polimi.emall.emsp.bookingmanagementservice.controller;
+
+import it.polimi.emall.emsp.bookingmanagementservice.generated.http.controller.TestApi;
+import it.polimi.emall.emsp.bookingmanagementservice.generated.http.model.DeliveringStatusDto;
+import it.polimi.emall.emsp.bookingmanagementservice.generated.http.model.TestComponentFeeDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import it.polimi.emall.emsp.bookingmanagementservice.generated.http.model.TestComponentDto;
+
+@RestController
+public class TestController implements TestApi {
+    @Override
+    public ResponseEntity<TestComponentDto> getTestComponent() {
+        return new ResponseEntity<>(
+                new TestComponentDto()
+                        .welcomeMesage("Hi!")
+                        .fee(new TestComponentFeeDto().value(2L).currency("EUR"))
+                        .status(new DeliveringStatusDto(3)),
+                HttpStatus.OK
+        );
+    }
+
+    @Override
+    public ResponseEntity<TestComponentDto> postTest(TestComponentDto testComponentDto) {
+        return new ResponseEntity<>(testComponentDto, HttpStatus.OK);
+    }
+}
---
+++ IT/BE/EMSP/BookingManagementService/src/test/java/it/polimi/emall/emsp/bookingmanagementservice/BookingManagementApplicationTests.java
@@ -0,0 +1,12 @@
+package it.polimi.emall.emsp.bookingmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class BookingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
