def0927379bc418e6904d64d8ed1f7fcd01fcd90
drgio <giorgiobuttiglieri99@gmail.com>
2023-01-24 13:29:58 +0100

    Add first draft of BookingService microservice

---
+++ IT/BE/CPMS/BookingService/src/test/java/it/polimi/cpms/bookingservice/BookingServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package it.polimi.cpms.bookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class BookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
