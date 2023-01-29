924c9175c69ab9ab9c696f799d817974cfd88f86
drgio <giorgiobuttiglieri99@gmail.com>
2023-01-29 01:04:01 +0100

    Configure primary ObjectMapper to support date serialization in standard format

--- IT/BE/EMSP/BookingManagementService/src/main/java/it/polimi/emall/emsp/bookingmanagementservice/configuration/JacksonObjectMapperConfig.java
+++ IT/BE/EMSP/BookingManagementService/src/main/java/it/polimi/emall/emsp/bookingmanagementservice/configuration/JacksonObjectMapperConfig.java
@@ -2,18 +2,22 @@ package it.polimi.emall.emsp.bookingmanagementservice.configuration;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.cfg.ConstructorDetector;
 import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class JacksonObjectMapperConfig {
     @Bean
     @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    @Primary
     public ObjectMapper jsonObjectMapper(){
         ObjectMapper mapper = JsonMapper.builder()
                 .constructorDetector(ConstructorDetector.USE_PROPERTIES_BASED)
@@ -26,6 +30,9 @@ public class JacksonObjectMapperConfig {
                 .enable( DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES );
 
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.setDateFormat(new StdDateFormat().withColonInTimeZone(true));
 
         return mapper;
     }
