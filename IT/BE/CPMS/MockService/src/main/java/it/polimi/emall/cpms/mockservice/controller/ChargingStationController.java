72d893c59bc7c0ac09c2a35945bb15f6dfffce3a
drgio <giorgiobuttiglieri99@gmail.com>
2023-01-29 11:36:14 +0100

    Add mock pricing [MockService]

--- IT/BE/CPMS/MockService/src/main/java/it/polimi/emall/cpms/mockservice/controller/ChargingStationController.java
+++ IT/BE/CPMS/MockService/src/main/java/it/polimi/emall/cpms/mockservice/controller/ChargingStationController.java
@@ -5,25 +5,30 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import it.polimi.emall.cpms.mockservice.configuration.JacksonObjectMapperConfig;
 import it.polimi.emall.cpms.mockservice.generated.http.server.controller.ChargingStationApi;
 import it.polimi.emall.cpms.mockservice.generated.http.server.model.ChargingStationDto;
+import it.polimi.emall.cpms.mockservice.generated.http.server.model.PricingDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class ChargingStationController implements ChargingStationApi {
+
     private final Resource chargingStationConfiguration;
+    private final Resource chargingStationPricing;
+
     private final ObjectMapper mapper = new JacksonObjectMapperConfig().jsonObjectMapper();
 
-    public ChargingStationController(@Value("classpath:chargingStations.json") Resource chargingStationConfiguration) {
+    public ChargingStationController(
+            @Value("classpath:chargingStations.json") Resource chargingStationConfiguration,
+            @Value("classpath:chargingStationsPricing.json") Resource chargingStationPricing
+    ) {
         this.chargingStationConfiguration = chargingStationConfiguration;
+        this.chargingStationPricing = chargingStationPricing;
     }
 
     @Override
@@ -31,7 +36,10 @@ public class ChargingStationController implements ChargingStationApi {
         try {
             List<ChargingStationDto> chargingStationList =
                     mapper.readValue(chargingStationConfiguration.getInputStream(), new TypeReference<>() {});
-            return new ResponseEntity<>(chargingStationList.get(0), HttpStatus.OK);
+            return new ResponseEntity<>(
+                    chargingStationList.stream().filter(station -> station.getChargingStationId().equals(chargingStationId))
+                            .findFirst().orElseThrow(),
+                    HttpStatus.OK);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -43,6 +51,29 @@ public class ChargingStationController implements ChargingStationApi {
             List<ChargingStationDto> chargingStationList =
                     mapper.readValue(chargingStationConfiguration.getInputStream(), new TypeReference<>() {});
             return new ResponseEntity<>(chargingStationList, HttpStatus.OK);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public ResponseEntity<PricingDto> getPricing(Long chargingStationId) {
+        try {
+            List<PricingDto> chargingStationPricingList =
+                    mapper.readValue(chargingStationPricing.getInputStream(), new TypeReference<>() {});
+            return new ResponseEntity<>(chargingStationPricingList.stream().filter(pricing -> pricing.getChargingStationId().equals(chargingStationId))
+                    .findFirst().orElseThrow(), HttpStatus.OK);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public ResponseEntity<List<PricingDto>> getPricingList() {
+        try {
+            List<PricingDto> chargingStationPricingList =
+                    mapper.readValue(chargingStationPricing.getInputStream(), new TypeReference<>() {});
+            return new ResponseEntity<>(chargingStationPricingList, HttpStatus.OK);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
---
+++ IT/BE/CPMS/MockService/src/main/java/it/polimi/emall/cpms/mockservice/controller/ChargingStationOverviewController.java
@@ -0,0 +1,9 @@
+package it.polimi.emall.cpms.mockservice.controller;
+
+import it.polimi.emall.cpms.mockservice.generated.http.server.controller.ChargingStationOverviewApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChargingStationOverviewController implements ChargingStationOverviewApi {
+
+}
