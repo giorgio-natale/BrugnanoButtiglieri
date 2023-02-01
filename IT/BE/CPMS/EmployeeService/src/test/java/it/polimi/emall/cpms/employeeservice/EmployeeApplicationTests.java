4fe67631d1f15f866d849168cc2b97b166b9282f
drgio <giorgiobuttiglieri99@gmail.com>
2023-02-01 12:02:12 +0100

    Add EmployeeService microservice

---
+++ IT/BE/CPMS/EmployeeService/src/test/java/it/polimi/emall/cpms/employeeservice/EmployeeApplicationTests.java
@@ -0,0 +1,12 @@
+package it.polimi.emall.cpms.employeeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
