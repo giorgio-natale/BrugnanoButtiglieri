933f5d504b491015f5b2302f16f14d9d087ccfd6
drgio <giorgiobuttiglieri99@gmail.com>
2023-02-01 11:49:19 +0100

    Add CustomerService microservice

---
+++ IT/BE/EMSP/CustomerService/src/test/java/it/polimi/emall/emsp/customerservice/CustomerApplicationTests.java
@@ -0,0 +1,12 @@
+package it.polimi.emall.emsp.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class CustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
