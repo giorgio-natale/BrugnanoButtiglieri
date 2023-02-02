6606d5bf43192f20ca11d5be77db26e929296a14
drgio <giorgiobuttiglieri99@gmail.com>
2023-02-02 14:49:21 +0100

    Allow a customer to cancel a booking[BookingManagementService]

---
+++ IT/BE/EMSP/BookingManagementService/src/main/java/it/polimi/emall/emsp/bookingmanagementservice/usecases/CancelABookingUseCase.java
@@ -0,0 +1,54 @@
+package it.polimi.emall.emsp.bookingmanagementservice.usecases;
+
+import it.polimi.emall.emsp.bookingmanagementservice.generated.http.client.cpms_bookingservice.endpoints.BookingApi;
+import it.polimi.emall.emsp.bookingmanagementservice.generated.http.client.cpms_bookingservice.model.BookingStatusCancelledClientDto;
+import it.polimi.emall.emsp.bookingmanagementservice.generated.http.server.model.BookingStatusCancelledDto;
+import it.polimi.emall.emsp.bookingmanagementservice.model.booking.Booking;
+import it.polimi.emall.emsp.bookingmanagementservice.model.booking.BookingManager;
+import it.polimi.emall.emsp.bookingmanagementservice.model.booking.BookingStatusEnum;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class CancelABookingUseCase {
+    private final BookingApi bookingApi;
+    private final BookingManager bookingManager;
+    private final TransactionTemplate transactionTemplate;
+
+    public CancelABookingUseCase(BookingApi bookingApi, BookingManager bookingManager, TransactionTemplate transactionTemplate) {
+        this.bookingApi = bookingApi;
+        this.bookingManager = bookingManager;
+        this.transactionTemplate = transactionTemplate;
+    }
+
+    public void cancelBooking(Long customerId, Long bookingId){
+        Booking booking = bookingManager.getEntityByKey(bookingId);
+        if(!booking.getCustomerId().equals(customerId))
+            throw new NoSuchElementException(String.format(
+                    "Cannot find booking %d for customer %d",
+                    bookingId,
+                    customerId
+            ));
+
+        if(!booking.getBookingStatus().getBookingStatus().equals(BookingStatusEnum.BookingStatusPlanned)){
+            throw new IllegalStateException(String.format(
+                    "Cannot close booking %s because it is in status %s",
+                    booking.getBookingCode(),
+                    booking.getBookingStatus().getBookingStatus()
+            ));
+        }
+
+        bookingApi.putBookingStatus(
+                        bookingId,
+                        new BookingStatusCancelledClientDto().bookingStatus(BookingStatusEnum.BookingStatusCancelled.name())
+                )
+                .block();
+
+        transactionTemplate.execute(status -> {
+            bookingManager.updateStatus(bookingManager.getEntityByKey(bookingId), new BookingStatusCancelledDto(bookingId));
+            return null;
+        });
+    }
+}
