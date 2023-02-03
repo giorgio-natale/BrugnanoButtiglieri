52e4941bdba697c40b42c158c11272f9ae8d5fc8
drgio <giorgiobuttiglieri99@gmail.com>
2023-02-03 13:22:25 +0100

    Fix booking cancelled[BookingService]

--- IT/BE/CPMS/BookingService/src/main/java/it/polimi/cpms/bookingservice/usecase/CancelBookingUseCase.java
+++ IT/BE/CPMS/BookingService/src/main/java/it/polimi/cpms/bookingservice/usecase/CancelBookingUseCase.java
@@ -4,16 +4,16 @@ import it.polimi.cpms.bookingservice.mappers.BookingMapper;
 import it.polimi.cpms.bookingservice.model.booking.Booking;
 import it.polimi.cpms.bookingservice.model.booking.BookingManager;
 import it.polimi.cpms.bookingservice.model.booking.BookingStatusEnum;
-import it.polimi.cpms.bookingservice.model.booking.dto.BookingKafkaDto;
 import it.polimi.cpms.bookingservice.model.booking.event.BookingUpdatedEvent;
 import it.polimi.emall.cpms.bookingservice.generated.http.client.chargingmanagementservice.endpoints.ChargingManagementApi;
-import it.polimi.emall.cpms.bookingservice.generated.http.client.chargingmanagementservice.model.SocketStatusClientDto;
-import it.polimi.emall.cpms.bookingservice.generated.http.server.model.BookingStatusInProgressDto;
+import it.polimi.emall.cpms.bookingservice.generated.http.server.model.BookingStatusCancelledDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Slf4j
 public class CancelBookingUseCase {
     private final BookingManager bookingManager;
     private final ApplicationEventPublisher applicationEventPublisher;
@@ -29,12 +29,14 @@ public class CancelBookingUseCase {
     @Transactional
     public void cancelBooking(Long bookingId){
         Booking booking = bookingManager.getEntityByKey(bookingId);
-        if(!booking.getBookingStatus().getBookingStatus().equals(BookingStatusEnum.BookingStatusPlanned))
+        if(!booking.getBookingStatus().getBookingStatus().equals(BookingStatusEnum.BookingStatusPlanned)) {
+            log.error("Booking with code {} is not planned", booking.getBookingCode());
             throw new IllegalStateException(String.format("Booking with code %s is not planned", booking.getBookingCode()));
+        }
 
         Booking updatedBooking = bookingManager.updateStatus(
                 bookingManager.getEntityByKey(bookingId),
-                new BookingStatusInProgressDto()
+                new BookingStatusCancelledDto()
                         .bookingId(bookingId)
         );
 
